1b29dc7f611b5c133d05404327b1c6e6c9e8c43d
santiago galván <sgalsant@gmail.com>
2020-03-27 17:35:42 +0000

    rebuild

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,24 +1,8 @@
 
-/*
-import * as App from '../dist/app.js';
+
+// import * as App from '../dist/app.js';
 
 
-
-describe ('testing sumjjj', function() {
-    it('suma de numeros positivos', function(){
-        chai.assert.equal(App.sum(3, 2), 5);
-    });
-   
-
-    it ('suma de negativos', function() {
-        chai.assert.equal(App.sum(-3, 2), -1);
-    });
-
-    it ('suma con log', function() {
-        chai.assert.equal(App.sumLog(2, 2), "resultado 4");
-    })
-})
-*/
 //https://www.chaijs.com/api/assert/
 
 describe ("mi grupo de test en el fichero app.test.ts", function() {

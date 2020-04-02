dd951f238f16902280ea201d944ef17d173dab57
santiago galván <sgalsant@gmail.com>
2020-04-02 23:01:36 +0100

    gitignore

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,16 @@ console.log('ejecutando fichero app.ts');
 export function main(): string {
   return hello();
 }
+
+export function serie(array: Array<number>):number {
+  if (array.length == 0) {
+      return 0;
+  }
+  let result: number;
+  result = (array[0]*array[1])+array[2];
+  for (let i = 3; i<array.length-1;i+=2) {
+    result = result *array[i]+array[i+1];
+  }
+  return result;
+
+}
--- test/app.test.ts
+++ test/app.test.ts
@@ -6,10 +6,14 @@ import assert from 'assert';
 // Descomentar si estas ejecutando desde navegador
 // const assert = chai.assert;
 
-import {main} from '../dist/app.js';
+import {serie} from '../dist/app.js';
 
 describe('mi grupo de test en el fichero app.test.ts', () => {
   it('un test', () => {
-    assert.equal(main(), 'hello');
+    assert.equal(serie([1,2,6,1,6,3,1,9,6]), 393);
+  });
+
+  it ('otro', function() {
+    assert.equal(serie([49, 92, 24, 76, 24, 78, 34, -3, 11, 90, 82, 66, -5, 85, 45, 20, 60, 20, 29, 85, 18, 6]), -1383675925019135700)
   });
 });

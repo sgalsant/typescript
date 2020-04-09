7e8f813fd191ff41ba8060045942f3fc0171a4af
santiago galván <sgalsant@gmail.com>
2020-04-09 14:09:55 +0100

    mocha exit

--- dist_test/app.test.d.ts
+++ dist_test/app.test.d.ts
@@ -1,1 +1,2 @@
-export {};
+/// <reference types="chai" />
+declare const assert: Chai.AssertStatic;
--- src/app.ts
+++ src/app.ts
@@ -1,77 +1,11 @@
-// Ejercicios de programación de array multidimensionales
-// para ejecutar los test, ejecutar en la línea de comandos: npm run test:build
+console.log('ejecutando fichero app.ts');
 
-// inicializar una matriz cuadrada con todos los valores a 10
-export function ejercicio1(d: number): number[][] | null {
-  return null;
-}
+/*
+  class App {
+    function main() {
+      console.log("ecutando app.main")
+    }
+  }
 
-// inicializar una matriz con valores crecientes
-export function ejercicio2(d: number): number[][] | null{
-  return null;
-}
-
-// generar una matriz identidad
-export function ejercicio3(d: number): number[][] | null{
-  return null;
-}
-
-// suma por filas de una matriz
-export function ejercicio4(matriz: number[][]): number[] | null {
-  return null;
-}
-
-// suma por columnas de una matriz
-export function ejercicio5(matriz: number[][]): number[] | null {
-  return null;
-}
-
-// suma de los elementos de una matriz de dos dimensiones
-export function ejercicio6(matriz: number[][]): number | null{
-  return null;
-}
-
-// suma de todos los elementos de una matriz de 3 dimensiones
-export function ejercicio7(matriz: number[][][]): number | null {
-  return null;
-}
-
-// suma de dos matrices
-export function ejercicio8(
-  matrizA: number[][],
-  matrizB: number[][]
-): number[][] | null {
-  return null;
-}
-
-// multiplicación de dos matrices
-export function ejercicio9(
-  matrizA: number[][],
-  matrizB: number[][]
-): number[][] | null{
-  return null;
-}
-
-// transponer una matriz
-export function ejercicio10(
-  matriz: number[][]
-): number[][] | null{
-   return null;
-}
-
-// invertir los elementos de cada fila
-export function ejercicio11(
-  matriz: number[][]
-): number[][] | null{
-  return null;
-}
-
-// rotar una matriz a la derecha
-export function ejercicio12(matriz: number[][]): number[][] | null{
-  return null;
-}
-
-// decodificar mensaje
-export function ejercicio13(texto: string[][], rejilla: number[][]): string | null {
-  return null
-}
+  new App().main();
+*/
